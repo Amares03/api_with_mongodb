774c187732db99eabaa9c9e736eaec2bf8833729
Amares03 <amaressabit0314@gmail.com>
2022-03-23 15:58:34 +0300

    schema creation

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const { MONGO_URI } = require('./config');
 const app = express();
 
 // connect to mongodb
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+})
 .then(()=>console.log(`MongoDb Connected...`))
 .catch(err =>console.log(err));
 
