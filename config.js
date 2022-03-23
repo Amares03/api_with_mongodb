f71053b3fc29b30494642c69270e52a653282893
Amares03 <amaressabit0314@gmail.com>
2022-03-23 15:48:27 +0300

    mongo connection

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI : 'mongodb+srv://amir:JYv18feDZnJo02M0@cluster0.itmlg.mongodb.net/amirdb?retryWrites=true&w=majority'
+};
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config');
 
 const app = express();
+
+// connect to mongodb
+mongoose.connect(MONGO_URI)
+.then(()=>console.log(`MongoDb Connected...`))
+.catch(err =>console.log(err));
 
 app.get('/',(req,res)=>{
     res.send(`hello world`);
